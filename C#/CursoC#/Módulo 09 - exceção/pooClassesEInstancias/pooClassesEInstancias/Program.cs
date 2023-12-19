c056c378a5da1bbaed100de26344c0b0cc408ae8
Juan Gabriel de Souza <150880787+juangabrieldesouza@users.noreply.github.com>
2023-12-19 04:03:10 +0100

    atualização

---
+++ C#/CursoC#/Módulo 09 - exceção/pooClassesEInstancias/pooClassesEInstancias/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooClassesEInstancias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Media minhaMedia; //Variavel do tipo Media, crianção de variavel/objeto
+                                             //é como se fosse uma variavel qualquer, igual o int
+                                            // porém o tipo Media, tem sua propria individualidade e função
+
+
+
+            minhaMedia = new Media(); //iniciação de variavel/objeto de "tipo Media". Instancia de Classe
+            //Exemplarização de classe. classe exemplar. intanciar 
+
+
+            Console.WriteLine(minhaMedia.calculoMedia(5, 7));
+
+            Media mediaDoJoao = new Media();
+
+            mediaDoJoao.calculoDeTamanho(); 
+        }
+    }
+    class Media
+    {
+        const double mediaBase = 2; //Propriedade da Classe. suas caracteristicas. Campo de Classe.
+
+        public double calculoMedia(int nota1, int nota2) //comportamento do método - calculoMedia - ou função
+        {
+            return ((nota1+nota2)/mediaBase); 
+        }
+
+        public void calculoDeTamanho()
+        {
+            string leitura = "Digite a sua altura em centímetros";
+            Console.WriteLine(leitura);
+            double altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("A sua altura é: " + altura / 100 + "m");
+        }
+
+    }
+
+        
+}
