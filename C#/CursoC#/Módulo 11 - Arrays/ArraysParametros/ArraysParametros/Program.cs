e31088be6a8c681901e641742183ce7bf786c179
Juan Gabriel de Souza <150880787+juangabrieldesouza@users.noreply.github.com>
2023-12-24 20:22:35 +0100

    comit Herança

--- C#/CursoC#/Módulo 11 - Arrays/ArraysParametros/ArraysParametros/Program.cs
+++ C#/CursoC#/Módulo 11 - Arrays/ArraysParametros/ArraysParametros/Program.cs
@@ -19,19 +19,43 @@ namespace ArraysParametros
 
             ProcessaDados(numeros); // método recebendo array
 
-            foreach(int i in numeros) //Array sendo exibida
+            foreach (int i in numeros) //Array sendo exibida
             {
                 Console.WriteLine(i);
             }
+
+
+            int[] array2 = LerDados();
+
+            foreach (int i in array2) Console.WriteLine(i);
         }
 
-        static void ProcessaDados(int[] dados)
+        static void ProcessaDados(int[] dados) //PARAMETRO QUE RECEBE ARRAY
         {
-            for(int i=0;i<4; i++) //indice
+            for (int i = 0; i < 4; i++) //indice
             {
                 dados[i] += 10; // somando + 10 em cada posição
 
             }
+        }
+
+        static int[] LerDados() // RECEBE VALORES E DEVOLVE ARRAY
+        {
+
+            Console.WriteLine("Quantos elementos você quer que o Array tenha?");
+
+            int numeroArray = int.Parse(Console.ReadLine());
+
+            int[] Elementos = new int[numeroArray]; 
+
+            for(int i=0;i<Elementos.Length; i++)
+            {
+                Console.WriteLine($"Digite o valor da posição {i}: ");
+                int resposta = int.Parse(Console.ReadLine());
+                Elementos[i] = resposta;
+            }
+
+            return Elementos;
         }
     }
 }
---
+++ C#/CursoC#/Módulo 12 - Herança (POO)/projetoHeranca/projetoHeranca/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoHeranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Gorila gorila = new Gorila("Pai do tarzan");
+
+            Cavalo cavalo = new Cavalo("Pé de pano");
+
+            Humano humano = new Humano("Juan");
+
+
+            Mamiferos animal = new Gorila("gogoda");
+
+            gorila.Andar();
+
+            humano.Andar();
+
+            cavalo.Andar();
+
+        }
+    }
+
+    class Mamiferos
+    {
+
+        public Mamiferos(string nome)
+        {
+            NomeSerVivo = nome;
+        }
+
+        public void Respirar()
+        {
+
+            Console.WriteLine("Estou respirando!");
+        }
+
+        public void getNome()
+        {
+
+            Console.WriteLine($"O nome do ser vivo é: {NomeSerVivo}");
+        }
+
+        public virtual void Andar()
+        {
+            Console.WriteLine("eu ando");
+        }
+
+        private string NomeSerVivo;      
+               
+    }
+
+    class Cavalo : Mamiferos
+    {
+
+        public Cavalo(string nomeCavalo) : base(nomeCavalo)
+        {
+
+        }
+
+        public void Andar()
+        {
+
+            Console.WriteLine("Sou capaz de trotar como cavalo");
+
+        }
+
+    }
+
+    class Humano : Mamiferos
+    {
+
+        public Humano(string nomeHumano): base(nomeHumano)
+        {
+
+
+        }
+
+        public void Andar()
+        {
+
+            Console.WriteLine("Eu ando em duas pernas");
+        }
+    }
+    
+    class Gorila : Mamiferos
+    {
+
+        public Gorila(string nomeGorila) : base(nomeGorila)
+        {
+
+
+        }
+
+
+        public void agarrar()
+        {
+
+            Console.WriteLine("Sou capaz de escalar arvores");
+        }
+    }
+}
