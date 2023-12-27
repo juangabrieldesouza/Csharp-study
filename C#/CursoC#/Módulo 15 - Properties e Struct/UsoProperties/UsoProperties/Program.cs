8d57b56de905282d99d4771da8afc245379c6611
Juan Gabriel de Souza <150880787+juangabrieldesouza@users.noreply.github.com>
2023-12-27 03:40:48 +0100

    Modulo 15 completo properties e struct

---
+++ C#/CursoC#/Módulo 15 - Properties e Struct/UsoProperties/UsoProperties/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F001 = new Funcionario("Juan");
+
+            F001.SALARIO = 1200;
+
+            Console.WriteLine("O salario do funcionario é: "+F001.SALARIO);
+
+
+        }
+    }
+
+    class Funcionario
+    {
+        public Funcionario (String nome)
+        {
+            this.nome = nome;
+
+        }
+
+        /*public void SetSalario(double salario)
+        {
+
+
+            if (salario < 0)
+            {
+                Console.WriteLine("O salario não pode ser negativo. O sálario foi registrado como 0");
+
+                this.salario = 0;
+            }
+            else
+            {
+                this.salario = salario;
+            }
+
+        }
+
+        public double GetSalario()
+        {
+            return salario;
+        }*/
+
+
+
+        //MÉTODO DE COMPORTAMENTO DA PROPRIEDADE
+
+        private double ConfereSalario(double salario)
+        {
+
+            if (salario < 0) return 0;
+
+            else return salario;
+
+        }
+//---------------------------------------
+        //CRIAÇÃO DA PROPRIEDADE
+
+       /* public double SALARIO
+        {
+            get { return this.salario; }
+
+            set { this.salario = ConfereSalario(value); }
+
+        }*/
+//---------------------------------------
+
+
+        //SIMPLIFICAÇÃO - Lambda
+
+        public double SALARIO
+        {
+            get => this.salario;
+
+            set => this.salario = ConfereSalario(value);
+        }
+
+        private string nome;
+
+        private double salario;
+
+       
+    }
+}
---
+++ C#/CursoC#/Módulo 15 - Properties e Struct/usoStruct/usoStruct/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usoStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F001 = new Funcionario(1200, 100);
+
+            F001.NovoSalario(F001, 200);
+
+            Console.WriteLine(F001);
+        }
+    }
+
+    public struct Funcionario
+    {
+
+        public double salarioBase, comissao;
+
+        public Funcionario(int salarioBase, int comissao)
+        {
+            this.salarioBase = salarioBase;
+
+            this.comissao = comissao;
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Salario e comissão do funcionário ({0},{1})", this.salarioBase, this.comissao);
+        }
+
+        public void NovoSalario(Funcionario func, int adicional)
+        {
+            func.salarioBase += adicional;
+
+            func.comissao += adicional;
+        }
+    }
+}
