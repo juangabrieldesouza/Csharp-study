ecf41b489a10fba7cbb614995fc0138b7ac20b99
Juan Gabriel de Souza <150880787+juangabrieldesouza@users.noreply.github.com>
2023-12-28 22:55:42 +0100

    Comit enum

---
+++ C#/CursoC#/Módulo 15 - Properties e Struct/UsoEnum/UsoEnum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoEnum
+{ 
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F001 = new Funcionario(Bonus.medio, 1500);
+
+
+            Console.WriteLine("O salario do funcionário é: "+ F001.getSalarioFuncionario());
+
+        }
+
+        class Funcionario
+        {
+            public Funcionario(Bonus Funcionario, double salario)
+            {
+                double BonusFunc = (double)Funcionario; //CASTING
+
+                this.salario = salario;
+
+                this.bonus = BonusFunc;
+            }
+
+            public double getSalarioFuncionario()
+            {
+
+                return salario + bonus;
+            }
+
+            private double bonus, salario;
+        }
+    }
+
+    enum Bonus {basico = 500, medio = 1500, pro = 3000 };
+}
---
+++ C#/CursoC#/Módulo 15 - Properties e Struct/UsoEnum/UsoEnum/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("UsoEnum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UsoEnum")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("c126d3f8-7932-4e5d-ad7e-0afe71d002fe")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
