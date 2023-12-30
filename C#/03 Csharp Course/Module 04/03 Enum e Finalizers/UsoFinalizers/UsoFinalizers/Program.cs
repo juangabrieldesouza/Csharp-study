6f8f62759d27dd1d4f2397f2216b13e6e6f0629d
Juan Gabriel de Souza <150880787+juangabrieldesouza@users.noreply.github.com>
2023-12-30 14:55:38 +0100

    Reoorganização

---
+++ C#/03 Csharp Course/Module 04/03 Enum e Finalizers/UsoFinalizers/UsoFinalizers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoFinalizers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class ControladorArquivos
+    {
+        StreamReader arquivo = null;
+
+        int linha = 0;
+
+        string leitor;
+
+        public ControladorArquivos()
+        {
+            arquivo = new StreamReader(@"D:\texto.txt");
+
+            while ((leitor = Console.ReadLine()) != null)
+            {
+
+                Console.WriteLine(leitor);
+
+                linha++;
+
+            }
+
+        }
+
+        public void Mensagem()
+        {
+            Console.WriteLine("Tem {0} linhas", linha);
+        }
+
+
+        ~ControladorArquivos()
+        {
+
+            arquivo.Close();
+
+        }
+    }
+}
---
+++ C#/03 Csharp Course/Module 05/01 Generics/01 Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+
+    class ObjectStorage
+    {
+        public ObjectStorage(int z)
+        {
+            ReadingElements = new Object[meter];
+        }
+
+        private Object[] ReadingElements;
+
+        private int meter = 0;
+    }
+}
