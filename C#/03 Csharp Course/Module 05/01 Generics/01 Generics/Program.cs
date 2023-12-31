52057b0d25e8903d1530ce25c166719e149a103e
Juan Gabriel de Souza <150880787+juangabrieldesouza@users.noreply.github.com>
2023-12-31 16:22:45 +0100

    Atualização

--- C#/03 Csharp Course/Module 05/01 Generics/01 Generics/Program.cs
+++ C#/03 Csharp Course/Module 05/01 Generics/01 Generics/Program.cs
@@ -10,21 +10,67 @@ namespace _01_Generics
     {
         static void Main(string[] args)
         {
-            
+            ObjectStorage<string> F1 = new ObjectStorage<string>(4);
+            ObjectStorage<Employees> F2 = new ObjectStorage<Employees>(5);
 
+            F2.insertObject(new Employees(2000));
+            F2.insertObject(new Employees(2700));
+            F2.insertObject(new Employees(3500));
+            F2.insertObject(new Employees(4700));
+            F2.insertObject(new Employees(6000));
 
+           F1.insertObject("Juan");
+            F1.insertObject("Antonio");
+            F1.insertObject("Joao");
+            F1.insertObject("Gabriel");
+
+            string NomeFuncionario = F1.GetElements(2);
+            Console.WriteLine(NomeFuncionario);
+
+            Employees SalaryEmp = (Employees)F2.GetElements(2);
+
+            Console.WriteLine(SalaryEmp.getSalary());
         }
     }
 
-    class ObjectStorage
+    class ObjectStorage<T>
     {
         public ObjectStorage(int z)
         {
-            ReadingElements = new Object[meter];
+            ReadingElements = new T[z];
         }
 
-        private Object[] ReadingElements;
+        public void insertObject(T obj)
+        {
+
+            ReadingElements[meter] = obj;
+
+            meter++;
+
+        }
+        public T GetElements(int i)
+        {
+            return ReadingElements[i];
+
+        }
+
+        private T[] ReadingElements;
 
         private int meter = 0;
+    }
+
+    class Employees
+    {
+        public Employees(double salary) 
+        {
+            this.salary = salary;
+        }
+
+        public double getSalary()
+        {
+            return salary;
+        }
+
+        private double salary;
     }
 }
---
+++ C#/03 Csharp Course/Module 05/GenericRestrictions/GenericRestrictions/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRestrictions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Director : IgetSalary
+    {
+        public Director(double salary)
+        {
+            this.salary = salary;
+        }
+
+        private double salary;
+
+        public double getSalary()
+        {
+            return salary;
+        }
+    }
+
+    class Secretary : IgetSalary
+    {
+        public Secretary(double salary)
+        {
+            this.salary = salary;
+        }
+
+        private double salary;
+
+        public double getSalary()
+        {
+            return salary;
+        }
+    }
+
+    class Eletrician : IgetSalary
+    {
+        public Eletrician(double salary)
+        {
+            this.salary = salary;
+        }
+
+        private double salary;
+
+        public double getSalary()
+        {
+            return salary;
+        }
+    }
+
+interface IgetSalary
+    {
+        double getSalary();
+    }
+
+}
