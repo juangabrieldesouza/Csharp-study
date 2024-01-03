65f19a8ffb1c4f9def056fa50bfc6181f57ed18a
Juan Gabriel de Souza <150880787+juangabrieldesouza@users.noreply.github.com>
2024-01-03 22:39:00 +0100

    Ex logic program
    Excercicios de logica (três atividades) realizadas

---
+++ C#/03 Csharp Course/Module 05/03 UsageList/UsageList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsageList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numeros = new List<int>(); // Declaração de collection do tipo List
+        }
+    }
+}
